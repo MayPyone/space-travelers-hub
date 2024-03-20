2422b56d4704bd9b0959f3df46e12d8a92693042
MayPyone <maypyone015@gmail.com>
2024-03-20 11:51:14 +0630

    Update dragon style

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -9,18 +9,26 @@ import planetLogo from './planet.png';
 function Navbar() {
   const [active, setActive] = useState(false); // Use boolean for active state
   const [icon, setIcon] = useState(false); // Use boolean for icon state
+
   const navToggle = () => {
     setActive(!active);
     setIcon(!icon);
   };
+
   const hideNav = () => {
     setActive(false);
     setIcon(false);
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      navToggle(); // Call the toggle function when the 'Enter' key is pressed
+    }
+  };
+
   return (
     <nav className={active ? 'nav nav--expanded' : 'nav'}>
-      <NavLink to="/" className="nav__brand">
+      <NavLink to="/" className="nav__brand" onKeyPress={handleKeyPress} tabIndex={0} role="button">
         <p className="logo">
           <img src={planetLogo} alt="planet-logo" className="logoImg" />
           <h1 className="title">Space Travelers&apos; Hub</h1>
@@ -31,16 +39,16 @@ function Navbar() {
           <NavLink to="/" className="navlink-class" onClick={hideNav}>Rockets</NavLink>
         </li>
         <li className="nav__item">
-          <NavLink to="mission" className="navlink-class" onClick={hideNav}>Missions</NavLink>
+          <NavLink to="/mission" className="navlink-class" onClick={hideNav}>Missions</NavLink>
         </li>
         <li className="nav__item">
-          <NavLink to="dragon" className="navlink-class" onClick={hideNav}>Dragons</NavLink>
+          <NavLink to="/dragon" className="navlink-class" onClick={hideNav}>Dragons</NavLink>
         </li>
         <li className="nav__item">
-          <NavLink to="profile" className="navlink-class" onClick={hideNav}>My Profile</NavLink>
+          <NavLink to="/profile" className="navlink-class" onClick={hideNav}>My Profile</NavLink>
         </li>
       </ul>
-      <div onClick={navToggle} className="nav__toggler.active ">
+      <div onClick={navToggle} onKeyPress={handleKeyPress} className={`nav__toggler ${active ? 'active' : ''}`} tabIndex={0} role="button">
         <div className="menugroup">
           <div className={icon ? 'nav__close' : 'menu'} id="nav-close">
             {icon ? <IoClose size={32} color="black" /> : <IoMenu size={32} color="black" />}
